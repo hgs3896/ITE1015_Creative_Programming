1364da175e6bbe9f9e9c48370fb970d8d2807740
2017030473 함지성 <2017030473@hanyang.ac.kr>
2017-09-15 22:50:27 +0900

    Correct some errors and comment what the codes do

--- HW1/Fibonacci.cc
+++ HW1/Fibonacci.cc
@@ -1,21 +1,34 @@
 #include <iostream>
 
-bool printed[45];
+unsigned long long *fib;
 
-unsigned int fibo(const unsigned int n){
-	int result = n<=2 ? 1 : fibo(n-2) + fibo(n-1);
-
-	if(!printed[n-1]){
-		std::cout << result << " ";
-		printed[n-1]=true;
+unsigned long long fibo(const unsigned int n){
+	if(n<=2){
+		if(!fib[n]){
+			std::cout << "1 ";
+			fib[n]=1ull;
+		}
+		return fib[n];
 	}
-
-	return result;
+	if(!fib[n-2]) fib[n-2] = fibo(n-2);
+	if(!fib[n-1]) fib[n-1] = fibo(n-1);
+	fib[n] = fib[n-2] + fib[n-1];
+	std::cout << fib[n] << " ";
+	return fib[n];
 }
 
 int main(){
 	int i, n;
 	std::cin >> n;
-	fibo(n);
+
+	try{
+		fib = new unsigned long long[n+1];
+		for(i=0;i<=n;++i) fib[i]=0ull;
+
+		fibo(n);
+
+		delete[] fib;
+	}catch(std::bad_alloc e){
+	}
 	std::cout << std::endl;
 }
--- HW1/magic_square.cc
+++ HW1/magic_square.cc
@@ -1,31 +1,55 @@
 #include <iostream>
 
-void make_a_fucking_magic_square(int N){
-	int **square = new int*[N];
-	int i,j,x,y;
-	for(i=0;i<N;++i)
-		square[i] = new int[N];
-	for(i=0;i<N*N;++i) square[i/N][i%N] = 0;
-	int v = 1;
-	i = 0, j = N/2;
+void make_a_fucking_magic_square(unsigned int** square, int N){
+	int i,j,x,y,v;
+
+	// Initialization
+	for(i=0;i<N*N;++i)	square[i/N][i%N] = 0;
+	v = 1, i = 0, j = N/2;
+	
+	// Fill in the fucking magic square the numbers from 1 to N*N.
 	while(v<=N*N){
+		// Fill in the number(the variable v).
 		square[i][j]=v++;
+		// The ordered pair (x,y) is the next point at which the next number of the square should be located.
 		x = (i - 1 + N) % N, y = (j + 1) % N;
 		if(!square[x][y]) i=x, j=y;
 		else i = (i+1) % N;
 	}
-	for(i=0;i<N;++i){
-		for(j=0;j<N;++j)
-			std::cout << square[i][j] << " ";
-		std::cout << std::endl;
-	}
-	for(i=0;i<N;++i)
-		delete[] square[i];
-	delete[] square;
 }
 
 int main(){
-	int N;
+	int i, N;
+	// Get an input data.
 	std::cin >> N;
-	make_a_fucking_magic_square(N);
+
+	// Check if N is something wrong.
+	if(N<=0 || N%2==0) return 0;
+
+	try{
+		// Allocation
+		unsigned int **square = new unsigned int*[N];
+		for(i=0;i<N;++i) square[i] = new unsigned int[N];
+
+		// Make a fucking magic square.
+		make_a_fucking_magic_square(square, N);
+
+		// Printing
+		for(i=0;i<N*N;++i){
+			std::cout << square[i/N][i%N];
+			if((i+1)%N==0)	std::cout << std::endl;
+			else 			std::cout << " ";
+		}
+
+		// Deallocation
+		for(i=0;i<N;++i){
+			delete[] square[i];
+			square[i] = NULL;
+		}
+		delete[] square;
+		square = NULL;
+	}catch(std::bad_alloc& e){
+		// The allocation was not that good.
+		std::cout << e.what();
+	}
 }
